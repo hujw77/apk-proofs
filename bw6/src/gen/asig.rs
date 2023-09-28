d311014c1daf5d563c5df333c965a425ca9365e8
echo <hujw77@gmail.com>
2023-09-28 16:44:27 +0800

    bls g2

--- bw6/src/gen/asig.rs
+++ bw6/src/gen/asig.rs
@@ -1,29 +1,12 @@
 use super::super::bls::Signature;
+use super::Bls12G2;
 use ark_ec::CurveGroup;
-use ark_ff::BigInteger;
-use ark_ff::PrimeField;
 use serde::Serialize;
-use serde_hex::{SerHexSeq, StrictPfx};
 use tinytemplate::TinyTemplate;
 
 #[derive(Serialize)]
 struct AggregateSignature {
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    h_x_c0: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    l_x_c0: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    h_x_c1: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    l_x_c1: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    h_y_c0: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    l_y_c0: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    h_y_c1: Vec<u8>,
-    #[serde(with = "SerHexSeq::<StrictPfx>")]
-    l_y_c1: Vec<u8>,
+    p: Bls12G2,
 }
 
 static ASIG_TEMPLATE: &str = "
@@ -31,18 +14,18 @@ static ASIG_TEMPLATE: &str = "
         return Bls12G2(
             Bls12Fp2(
                 Bls12Fp(
-					{h_x_c0}, {l_x_c0}
+					{p.x.c0.a}, {p.x.c0.b}
                 ),
                 Bls12Fp(
-					{h_x_c1}, {l_x_c1}
+					{p.x.c1.a}, {p.x.c1.b}
                 )
             ),
             Bls12Fp2(
                 Bls12Fp(
-					{h_y_c0}, {l_y_c0}
+					{p.y.c0.a}, {p.y.c0.b}
                 ),
                 Bls12Fp(
-					{h_y_c1}, {l_y_c1}
+					{p.y.c1.a}, {p.y.c1.b}
                 )
             )
         );
@@ -54,27 +37,9 @@ pub fn print_asig(asig: Signature) {
     tt.add_template("asig", ASIG_TEMPLATE).unwrap();
 
     let sig = asig.as_ref().into_affine();
-    let x_c0 = sig.x.c0.into_bigint().to_bytes_be();
-    let (h_x_c0, l_x_c0) = x_c0.split_at(x_c0.len() - 32);
-
-    let x_c1 = sig.x.c1.into_bigint().to_bytes_be();
-    let (h_x_c1, l_x_c1) = x_c1.split_at(x_c1.len() - 32);
-
-    let y_c0 = sig.y.c0.into_bigint().to_bytes_be();
-    let (h_y_c0, l_y_c0) = y_c0.split_at(y_c0.len() - 32);
-
-    let y_c1 = sig.y.c1.into_bigint().to_bytes_be();
-    let (h_y_c1, l_y_c1) = y_c1.split_at(y_c1.len() - 32);
 
     let context = AggregateSignature {
-        h_x_c0: h_x_c0.to_vec(),
-        l_x_c0: l_x_c0.to_vec(),
-        h_x_c1: h_x_c1.to_vec(),
-        l_x_c1: l_x_c1.to_vec(),
-        h_y_c0: h_y_c0.to_vec(),
-        l_y_c0: l_y_c0.to_vec(),
-        h_y_c1: h_y_c1.to_vec(),
-        l_y_c1: l_y_c1.to_vec(),
+        p: Bls12G2::from(sig),
     };
 
     let rendered = tt.render("asig", &context).unwrap();
